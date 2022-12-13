f645e2526b0eb83ed8db2caa07f87d01a16ddb4e
Burak Senturk <senturkburk@gmail.com>
2022-12-13 12:07:22 +0300

    first commit

---
+++ js/saat.js
@@ -0,0 +1,22 @@
+let Name = prompt("lütfen adinizi giriniz")
+let NewName = document.querySelector("#myName")
+NewName.innerHTML=`${Name}`
+
+
+const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+function showTime() {
+    let dateTime = new Date();
+    let day = days[dateTime.getDay()];
+    let hour = dateTime.getHours();
+    let minute = dateTime.getMinutes();
+    let second = dateTime.getSeconds();
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    second = (second < 10) ? "0" + second : second;
+
+    document.querySelector("#myClock").innerHTML = `${hour}: ${minute}: ${second}  ${day}`
+}
+
+setInterval(showTime, 1000);
+showTime();
